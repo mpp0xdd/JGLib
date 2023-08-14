cfb49f3e237d45cc56d7b0bfd4b7bee975f91175
mpp <mpp0xdd@gmail.com>
2023-08-14 20:52:35 +0900

    テストコードを改善した

--- src/jglib/test/TestExecutor.java
+++ src/jglib/test/TestExecutor.java
@@ -1,10 +1,15 @@
 package jglib.test;
 
 public class TestExecutor {
+
+  private static boolean complete() {
+    System.err.println();
+    System.err.println("All tests completed successfully.");
+    return true;
+  }
+
   public static void main(String[] args) {
     Test.invokeTestClass(new SpriteSheetTest());
-
-    System.err.println();
-    assert false : "All tests completed successfully.";
+    assert complete();
   }
 }
