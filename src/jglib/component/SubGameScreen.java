3c36dafbe2babcb0c0e097562c01e79b9c8a3d09
mpp <mpp0xdd@gmail.com>
2024-02-25 10:12:30 +0900

    javadoc修正

--- src/jglib/component/SubGameScreen.java
+++ src/jglib/component/SubGameScreen.java
@@ -2,15 +2,29 @@ package jglib.component;
 
 import java.awt.Graphics;
 
+/**
+ * サブゲーム画面を表すインターフェイスです。
+ *
+ * @author mpp
+ */
 public interface SubGameScreen {
 
+  /** @return サブゲーム画面のx座標。 */
   int x();
 
+  /** @return サブゲーム画面のy座標。 */
   int y();
 
+  /** @return サブゲーム画面の横幅。 */
   int width();
 
+  /** @return サブゲーム画面の縦幅。 */
   int height();
 
+  /**
+   * サブゲーム画面を描画します。
+   *
+   * @param g サブゲーム画面の描画に使用するグラフィックスコンテキスト。
+   */
   void draw(Graphics g);
 }
