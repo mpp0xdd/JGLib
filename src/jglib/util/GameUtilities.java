8927687eaa9aea38b6f554d458c0a57f2272e373
mpp <mpp0xdd@gmail.com>
2024-05-18 23:13:22 +0900

    StringDrawerを作成

--- src/jglib/util/GameUtilities.java
+++ src/jglib/util/GameUtilities.java
@@ -1,7 +1,5 @@
 package jglib.util;
 
-import java.awt.FontMetrics;
-import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -36,63 +34,6 @@ public final class GameUtilities {
       TimeUnit.MILLISECONDS.sleep(millis);
     } catch (InterruptedException ie) {
       ie.printStackTrace();
-    }
-  }
-
-  /**
-   * 座標(x, y) を左上隅として文字列(複数行) を描画します。
-   *
-   * @param g 文字列の描画に使用するグラフィックスコンテキスト。
-   * @param x 文字列の左上隅のx座標。
-   * @param y 文字列の左上隅のy座標。
-   * @param lines 描画する文字列。
-   */
-  public static void drawString(Graphics g, int x, int y, String... lines) {
-    FontMetrics fontMetrics = g.getFontMetrics();
-    final int height = fontMetrics.getMaxDescent() + fontMetrics.getMaxAscent();
-
-    y += fontMetrics.getMaxAscent();
-    for (String line : lines) {
-      g.drawString(line, x, y);
-      y += height;
-    }
-  }
-
-  /**
-   * 座標(x, y) を右上隅として文字列(複数行) を描画します。
-   *
-   * @param g 文字列の描画に使用するグラフィックスコンテキスト。
-   * @param x 文字列の右上隅のx座標。
-   * @param y 文字列の右上隅のy座標。
-   * @param lines 描画する文字列。
-   */
-  public static void drawStringFromTopRight(Graphics g, int x, int y, String... lines) {
-    FontMetrics fontMetrics = g.getFontMetrics();
-    final int height = fontMetrics.getMaxDescent() + fontMetrics.getMaxAscent();
-
-    y += fontMetrics.getMaxAscent();
-    for (String line : lines) {
-      g.drawString(line, x - fontMetrics.stringWidth(line), y);
-      y += height;
-    }
-  }
-
-  /**
-   * 座標(x, y) を中心として文字列(複数行) を描画します。
-   *
-   * @param g 文字列の描画に使用するグラフィックスコンテキスト。
-   * @param x 文字列の左上隅のx座標。
-   * @param y 文字列の左上隅のy座標。
-   * @param lines 描画する文字列。
-   */
-  public static void drawStringAfterCentering(Graphics g, int x, int y, String... lines) {
-    FontMetrics fontMetrics = g.getFontMetrics();
-    final int height = fontMetrics.getMaxDescent() + fontMetrics.getMaxAscent();
-
-    y += (fontMetrics.getMaxAscent() - fontMetrics.getMaxDescent()) / 2;
-    for (String line : lines) {
-      g.drawString(line, x - fontMetrics.stringWidth(line) / 2, y);
-      y += height;
     }
   }
 
---
+++ src/jglib/util/StringDrawer.java
@@ -0,0 +1,67 @@
+package jglib.util;
+
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+
+/**
+ * StringDrawer列挙型は，文字列のドロワーを表します。
+ *
+ * @author mpp
+ */
+public enum StringDrawer {
+  /** 左揃えで文字列を描画するドロワーです。 */
+  LEFT {
+    @Override
+    public void draw(Graphics g, int x, int y, String... lines) {
+      final FontMetrics fontMetrics = g.getFontMetrics();
+      final int height = fontMetrics.getMaxDescent() + fontMetrics.getMaxAscent();
+
+      y += fontMetrics.getMaxAscent();
+      for (String line : lines) {
+        g.drawString(line, x, y);
+        y += height;
+      }
+    }
+  },
+
+  /** 中央揃えで文字列を描画するドロワーです。 */
+  CENTER {
+    @Override
+    public void draw(Graphics g, int x, int y, String... lines) {
+      final FontMetrics fontMetrics = g.getFontMetrics();
+      final int height = fontMetrics.getMaxDescent() + fontMetrics.getMaxAscent();
+
+      y += (fontMetrics.getMaxAscent() - fontMetrics.getMaxDescent()) / 2;
+      for (String line : lines) {
+        g.drawString(line, x - fontMetrics.stringWidth(line) / 2, y);
+        y += height;
+      }
+    }
+  },
+
+  /** 右揃えで文字列を描画するドロワーです。 */
+  RIGHT {
+    @Override
+    public void draw(Graphics g, int x, int y, String... lines) {
+      final FontMetrics fontMetrics = g.getFontMetrics();
+      final int height = fontMetrics.getMaxDescent() + fontMetrics.getMaxAscent();
+
+      y += fontMetrics.getMaxAscent();
+      for (String line : lines) {
+        g.drawString(line, x - fontMetrics.stringWidth(line), y);
+        y += height;
+      }
+    }
+  },
+  ;
+
+  /**
+   * このドロワーを使用して座標(x, y) に文字列(複数行) を描画します。
+   *
+   * @param g 文字列の描画に使用するグラフィックスコンテキスト。
+   * @param x 文字列のx座標。
+   * @param y 文字列のy座標。
+   * @param lines 描画する文字列。
+   */
+  public abstract void draw(Graphics g, int x, int y, String... lines);
+}
