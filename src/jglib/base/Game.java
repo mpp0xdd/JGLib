74be29a6668a428d9f6f435faaf1340e4cb56679
mpp <mpp0xdd@gmail.com>
2024-06-17 23:19:16 +0900

    WIP

--- src/jglib/base/Game.java
+++ src/jglib/base/Game.java
@@ -2,6 +2,17 @@ package jglib.base;
 
 public abstract class Game {
 
+  public static void launch(Class<? extends Game> gameClass) {
+    try {
+      Game game = gameClass.getDeclaredConstructor().newInstance();
+      game.start();
+    } catch (Exception e) {
+      GameError error = new GameError(e);
+      GameLogger.getLogger().error(error);
+      System.exit(1);
+    }
+  }
+
   protected Game() {
     Thread.currentThread().setUncaughtExceptionHandler(new UncaughtGameExceptionHandler());
   }
