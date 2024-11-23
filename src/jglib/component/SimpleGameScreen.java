4d6cd43c4a1859f4861093246a673e779700e17d
mpp <mpp0xdd@gmail.com>
2024-11-23 19:09:39 +0900

    WIP

--- src/jglib/component/SimpleGameScreen.java
+++ src/jglib/component/SimpleGameScreen.java
@@ -1,8 +1,17 @@
 package jglib.component;
 
+import java.awt.Graphics;
+
 /**
  * 更新機能を持たない単純なゲーム画面の作成を円滑に進めていく為に用意された抽象基底クラスです。
  *
  * @author mpp
  */
-public abstract non-sealed class SimpleGameScreen extends GameScreenBase {}
+public abstract non-sealed class SimpleGameScreen extends GameScreenBase {
+  
+  @Override
+  protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    this.paintGameComponent(g);
+  }
+}
