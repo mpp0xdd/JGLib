12a97b1915b3dc57e8c97d9183571ffe55837868
mpp <mpp0xdd@gmail.com>
2024-11-24 12:34:17 +0900

    Add Rectangular

---
+++ src/jglib/util/spec/Rectangular.java
@@ -0,0 +1,26 @@
+package jglib.util.spec;
+
+import java.awt.Rectangle;
+
+public interface Rectangular {
+
+  int x();
+
+  int y();
+
+  int width();
+
+  int height();
+
+  default Rectangle asRectangle() {
+    return new Rectangle(x(), y(), width(), height());
+  }
+
+  default boolean contains(Rectangular other) {
+    return this.asRectangle().contains(other.asRectangle());
+  }
+
+  default boolean intersects(Rectangular other) {
+    return this.asRectangle().intersects(other.asRectangle());
+  }
+}
