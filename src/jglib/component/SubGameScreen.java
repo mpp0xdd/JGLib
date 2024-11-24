a892a55a0d34436cb0119276022e1c0000b6f22c
mpp <mpp0xdd@gmail.com>
2024-11-24 12:37:52 +0900

    WIP

--- src/jglib/component/SubGameScreen.java
+++ src/jglib/component/SubGameScreen.java
@@ -2,24 +2,29 @@ package jglib.component;
 
 import java.awt.Graphics;
 import jglib.util.spec.Drawable;
+import jglib.util.spec.Rectangular;
 
 /**
  * サブゲーム画面を表すインターフェイスです。
  *
  * @author mpp
  */
-public interface SubGameScreen extends Drawable {
+public interface SubGameScreen extends Rectangular, Drawable {
 
   /** @return サブゲーム画面のx座標。 */
+  @Override
   int x();
 
   /** @return サブゲーム画面のy座標。 */
+  @Override
   int y();
 
   /** @return サブゲーム画面の横幅。 */
+  @Override
   int width();
 
   /** @return サブゲーム画面の縦幅。 */
+  @Override
   int height();
 
   /**
