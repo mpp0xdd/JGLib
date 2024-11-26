c0079db44648707dbc0326b8a0106d0de3a65359
mpp <mpp0xdd@gmail.com>
2024-11-27 00:27:11 +0900

    Fix test

--- src/jglib/test/Test.java
+++ src/jglib/test/Test.java
@@ -1,52 +1,14 @@
 package jglib.test;
 
-import static java.util.Objects.requireNonNull;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.lang.reflect.Method;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @FunctionalInterface
 interface Test {
 
-  void test() throws Exception;
-
-  public static void invokeTestClass(Object object) {
-    requireNonNull(object);
-    if (!object.getClass().isAnnotationPresent(TestClass.class)) {
-      System.err.println("[ERROR] Not a test class, so cannot be invoked: " + object.getClass());
-      System.exit(1);
-    }
-
-    Method[] declaredMethods = requireNonNull(object.getClass().getDeclaredMethods());
-    List<Method> testMethods =
-        Stream.of(declaredMethods)
-            .filter(method -> method.isAnnotationPresent(TestMethod.class))
-            .collect(Collectors.toList());
-
-    if (testMethods.isEmpty()) {
-      System.err.println("[ERROR] Test method not found: " + object.getClass());
-      System.exit(1);
-    }
-
-    try {
-      System.err.printf("> Start %s%n", object.getClass().getSimpleName());
-      for (Method testMethod : testMethods) {
-        System.err.printf(String.format(">> %s()", testMethod.getName()));
-        testMethod.invoke(object);
-        System.err.println(" completed successfully.");
-      }
-      System.err.printf("> %s completed successfully.%n", object.getClass().getSimpleName());
-      System.err.println();
-    } catch (Exception e) {
-      e.printStackTrace();
-      System.exit(1);
-    }
-  }
+  void test() throws Throwable;
 
   @Target({ElementType.TYPE})
   @Retention(RetentionPolicy.RUNTIME)
--- src/jglib/test/TestExecutor.java
+++ src/jglib/test/TestExecutor.java
@@ -1,24 +1,64 @@
 package jglib.test;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import jglib.test.Test.TestClass;
+import jglib.test.Test.TestMethod;
 
 class TestExecutor {
 
-  private final List<Object> testClasses;
+  private final List<Class<?>> testClasses;
 
   public TestExecutor() {
     testClasses = new ArrayList<>();
   }
 
-  public void register(Object testClass) {
+  public void register(Class<?> testClass) {
     testClasses.add(testClass);
   }
 
   public void execute() {
     checkEnableAssertions();
-    testClasses.forEach(Test::invokeTestClass);
+    testClasses.forEach(this::instantiateAndExecute);
     done();
+  }
+
+  private <T> void instantiateAndExecute(Class<T> testClass) {
+    if (!testClass.isAnnotationPresent(TestClass.class)) {
+      throw new RuntimeException("Not a test class: " + testClass);
+    }
+
+    T testInstance;
+    try {
+      testInstance = testClass.getDeclaredConstructor().newInstance();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
+    final List<Method> testMethods =
+        Stream.of(testClass.getDeclaredMethods())
+            .filter(method -> method.isAnnotationPresent(TestMethod.class))
+            .collect(Collectors.toUnmodifiableList());
+
+    if (testMethods.isEmpty()) {
+      throw new RuntimeException("Test method not found: " + testClass);
+    }
+
+    try {
+      System.err.printf("> Start %s%n", testClass.getSimpleName());
+      for (Method testMethod : testMethods) {
+        System.err.printf(String.format(">> %s()", testMethod.getName()));
+        testMethod.invoke(testInstance);
+        System.err.println(" completed successfully.");
+      }
+      System.err.printf("> %s completed successfully.%n", testClass.getSimpleName());
+      System.err.println();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
   }
 
   private void checkEnableAssertions() {
@@ -36,12 +76,12 @@ class TestExecutor {
   public static void main(String[] args) {
     TestExecutor executor = new TestExecutor();
 
-    executor.register(new GameTest());
-    executor.register(new SpriteSheetTest());
-    executor.register(new StringDrawerTest());
-    executor.register(new StopwatchTest());
-    executor.register(new FrameRateTest());
-    executor.register(new KeyboardTest());
+    executor.register(GameTest.class);
+    executor.register(SpriteSheetTest.class);
+    executor.register(StringDrawerTest.class);
+    executor.register(StopwatchTest.class);
+    executor.register(FrameRateTest.class);
+    executor.register(KeyboardTest.class);
 
     executor.execute();
   }
