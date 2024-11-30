b674525e4a965338a7c4a9a4b97748b86bd1e942
mpp <mpp0xdd@gmail.com>
2024-11-30 16:29:43 +0900

    Improve javadoc

--- src/jglib/core/Game.java
+++ src/jglib/core/Game.java
@@ -3,8 +3,20 @@ package jglib.core;
 import java.util.Optional;
 import jglib.service.logging.GameLoggingService;
 
+/**
+ * 全てのゲームの抽象基底クラスです。
+ *
+ * @author mpp
+ */
 public abstract class Game {
 
+  /**
+   * ゲームを起動します。
+   *
+   * @param <T> ゲーム
+   * @param gameClass ゲームの型
+   * @return 起動したゲーム。起動に失敗した場合は空
+   */
   public static <T extends Game> Optional<T> launch(Class<T> gameClass) {
     T game;
 
@@ -20,9 +32,11 @@ public abstract class Game {
     return Optional.ofNullable(game);
   }
 
+  /** デフォルトコンストラクタです。 */
   protected Game() {
     Thread.currentThread().setUncaughtExceptionHandler(new UncaughtGameExceptionHandler());
   }
 
+  /** このゲームの開始処理を行います。 */
   protected abstract void start();
 }
