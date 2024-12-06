97aae4afcbc496d55e35e05bebb4f55d612d9ba8
mpp <mpp0xdd@gmail.com>
2024-12-07 00:35:06 +0900

    Revert "WIP"
    This reverts commit 8c13032addb8b82eaea83e82e230b7fb52794192.

--- src/jglib/test/ObjectAssert.java
+++ src/jglib/test/ObjectAssert.java
@@ -2,19 +2,19 @@ package jglib.test;
 
 import java.util.Objects;
 
-class Assert<T> {
+class ObjectAssert<T> {
 
   static {
     Assertions.requireEnable();
   }
 
-  public static <T> Assert<T> of(T actual) {
-    return new Assert<T>(actual);
+  public static <T> ObjectAssert<T> of(T actual) {
+    return new ObjectAssert<T>(actual);
   }
 
   private final T actual;
 
-  private Assert(T actual) {
+  private ObjectAssert(T actual) {
     this.actual = actual;
   }
 
